ec4a55d2d782b5ff86a039dffe733967d45e21ba
avimis92 <avim92@gmail.com>
2020-04-16 17:13:12 +0300

    middlewares added to project

---
+++ src/middleware/error.js
@@ -0,0 +1,16 @@
+// Handle Errors
+const winston = require('winston');
+
+module.exports = function(err, req, res, next){
+    // Log the exception
+    winston.error(err.message, err);
+
+    // error
+    // warn
+    // info
+    // verbose
+    // debug
+    // silly
+
+    res.status(500).send('Something failed.');
+}
