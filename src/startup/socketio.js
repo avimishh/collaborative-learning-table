b2ada6b87056a4b612eca34135cc37c428ea7f7f
avimis92 <avim92@gmail.com>
2020-05-22 14:39:15 +0300

    folders clean

--- src/startup/socketio.js
+++ src/startup/socketio.js
@@ -5,14 +5,6 @@ const socketio = require('socket.io');
 
 module.exports = function (server, app) {
     const io = socketio(server);
-    // var session = require("express-session")({
-    //     secret: "my-secret",
-    //     resave: true,
-    //     saveUninitialized: true
-    //   });
-    // var sharedsession = require("express-socket.io-session");
-    // app.use(session);
-    
     console.log('io is on');
     require('./../public/games/gamingServer').init(io);
     // require('./../public/server_math/server')(io);
