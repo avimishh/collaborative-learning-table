9b4ecfeddff373e19bd6176fb977f80ffa24b3f3
avimishh <avim92@gmail.com>
2020-11-06 22:16:30 +0200

    ajax requests to api

--- client/api.js
+++ client/api.js
@@ -1,16 +1,39 @@
-Webimport { getServerApiUrl } from "/globalDeclarations.js";
+import {
+    getServerApiUrl
+} from "/globalDeclarations.js";
 
 const server = getServerApiUrl();
+const authApi = server + "auth/";
 const childsApi = server + "childs/";
-const statsApi = server + "stats/";
-const authParentApi = server + "auth/parent/";
-const usersApi = server + "users/";
 const parentsApi = server + "parents/";
+const teachersApi = server + "teachers/";
+const statsApi = server + "stats/";
 const notesApi = server + "notes/";
+const gamesApi = server + "games/";
 
 $.ajaxSetup({
     contentType: "application/json"
 });
+
+function getApiRouteByUserType(userType) {
+    let urlApi;
+    switch (userType) {
+        case "child":
+            urlApi = childsApi;
+            break;
+        case "teacher":
+            urlApi = teachersApi;
+            break;
+        case "parent":
+            urlApi = parentsApi;
+            break;
+        default:
+            break;
+    }
+    return urlApi;
+}
+
+//#region Login Requests
 
 export function childLoginRequest(childId, gamesPassword, successFunction, errorFunction) {
     $.ajax({
@@ -26,9 +49,9 @@ export function childLoginRequest(childId, gamesPassword, successFunction, error
     });
 }
 
-export function userLoginRequest(userId, userPassword, successFunction, errorFunction) {
-    let request = {
-        url: authParentApi,
+export function userLoginRequest(userType, userId, userPassword, successFunction, errorFunction) {
+    $.ajax({
+        url: authApi + userType,
         method: "POST",
         data: JSON.stringify({
             id: userId,
@@ -41,10 +64,242 @@ export function userLoginRequest(userId, userPassword, successFunction, errorFun
         error: function (xhr) {
             errorFunction(xhr.status, xhr.responseText);
         }
-    };
-    $.ajax(request);
+    });
+}
+//#endregion
+
+//#region Register Requests
+
+export function userRegisterRequest(userType, firstName, lastName, id, password, phone, successFunction, errorFunction) {
+    $.ajax({
+        url: (userType === "teacher") ? teachersApi : parentsApi,
+        method: "POST",
+        // headers: { 'x-auth-token' : localStorage.getItem('token') },
+        data: JSON.stringify({
+            firstName,
+            lastName,
+            id,
+            password,
+            phone
+        }),
+        success: function (data, textStatus, xhr) {
+            if (userType === "parent")
+                localStorage.setItem("token", xhr.getResponseHeader("x-auth-token"));
+            successFunction(data);
+        },
+        error: function (xhr) {
+            errorFunction(xhr.status, xhr.responseText);
+        },
+    });
+}
+//#endregion
+
+
+//#region Get Model Data Requests
+
+export function getMeUserRequest(userType, successFunction, errorFunction) {
+    $.ajax({
+        url: getApiRouteByUserType(userType) + "me/",
+        method: "GET",
+        headers: {
+            'x-auth-token': localStorage.getItem('token')
+        },
+        success: function (data, textStatus, xhr) {
+            successFunction(data);
+        },
+        error: function (xhr) {
+            errorFunction(xhr.status, xhr.responseText);
+        }
+    });
 }
 
+export function getChildByIdRequest(childId, successFunction, errorFunction) {
+    getModelObjectByIdRequest("child", childId, successFunction, errorFunction)
+}
+
+export function getTeacherByIdRequest(teacherId, successFunction, errorFunction) {
+    getModelObjectByIdRequest("teacher", teacherId, successFunction, errorFunction)
+}
+
+export function getParentByIdRequest(parentId, successFunction, errorFunction) {
+    getModelObjectByIdRequest("parent", parentId, successFunction, errorFunction)
+}
+
+export function getChildsRequest(successFunction, errorFunction) {
+    getModelObjectsRequest("child", successFunction, errorFunction)
+}
+
+export function getTeachersRequest(successFunction, errorFunction) {
+    getModelObjectsRequest("teacher", successFunction, errorFunction)
+}
+
+export function getParentsRequest(successFunction, errorFunction) {
+    getModelObjectsRequest("parent", successFunction, errorFunction)
+}
+
+function getModelObjectByIdRequest(userType, userId, successFunction, errorFunction) {
+    $.ajax({
+        url: getApiRouteByUserType(userType) + userId,
+        method: "GET",
+        headers: {
+            "x-auth-token": localStorage.getItem("token")
+        },
+        success: function (data, textStatus, xhr) {
+            successFunction(data);
+        },
+        error: function (xhr) {
+            errorFunction(xhr.status, xhr.responseText);
+        },
+    });
+}
+
+function getModelObjectsRequest(userType, successFunction, errorFunction) {
+    $.ajax({
+        url: getApiRouteByUserType(userType),
+        method: "GET",
+        headers: {
+            'x-auth-token': localStorage.getItem('token')
+        },
+        success: function (data, textStatus, xhr) {
+            successFunction(data);
+        },
+        error: function (xhr) {
+            errorFunction(xhr.status, xhr.responseText);
+        }
+    });
+}
+//#endregion
+
+
+//#region Change Model Data Requests
+
+export function updateChildByIdRequest(childId, updatedChild, successFunction, errorFunction) {
+    updateModelObjectByIdRequest("child", childId, updatedChild, successFunction, errorFunction)
+}
+
+export function updateTeacherByIdRequest(teacherId, updatedTeacher, successFunction, errorFunction) {
+    updateModelObjectByIdRequest("teacher", teacherId, updatedTeacher, successFunction, errorFunction)
+}
+
+export function updateParentByIdRequest(parentId, updatedParent, successFunction, errorFunction) {
+    updateModelObjectByIdRequest("parent", parentId, updatedParent, successFunction, errorFunction)
+}
+
+export function updateTeacherPasswordByIdRequest(teacherId, newPassword, successFunction, errorFunction) {
+    updateUserPasswordByIdRequest("teacher", teacherId, newPassword, successFunction, errorFunction)
+}
+
+export function updateParentPasswordByIdRequest(parentId, newPassword, successFunction, errorFunction) {
+    updateUserPasswordByIdRequest("parent", parentId, newPassword, successFunction, errorFunction)
+}
+
+export function deleteChildByIdRequest(childId, successFunction, errorFunction) {
+    deleteModelObjectByIdRequest("child", childId, successFunction, errorFunction)
+}
+
+export function deleteTeacherByIdRequest(teacherId, successFunction, errorFunction) {
+    deleteModelObjectByIdRequest("teacher", teacherId, successFunction, errorFunction)
+}
+
+export function deleteParentByIdRequest(parentId, successFunction, errorFunction) {
+    deleteModelObjectByIdRequest("parent", parentId, successFunction, errorFunction)
+}
+
+function updateModelObjectByIdRequest(userType, userId, objectToUpdate, successFunction, errorFunction) {
+    $.ajax({
+        url: getApiRouteByUserType(userType) + userId,
+        method: "PUT",
+        headers: {
+            'x-auth-token': localStorage.getItem('token')
+        },
+        data: JSON.stringify(objectToUpdate),
+        success: function (data, textStatus, xhr) {
+            // localStorage.setItem('teacher', JSON.stringify(data));
+            // Store Object data in localStorage
+            localStorage.setItem(userType, JSON.stringify(data));
+            successFunction();
+        },
+        error: function (xhr) {
+            errorFunction(xhr.status, xhr.responseText);
+        }
+    });
+}
+
+function updateUserPasswordByIdRequest(userType, userId, newPassword, successFunction, errorFunction) {
+    $.ajax({
+        url: getApiRouteByUserType(userType) + 'changePassword/' + userId,
+        method: "PUT",
+        headers: {
+            'x-auth-token': localStorage.getItem('token')
+        },
+        data: JSON.stringify({
+            newPassword
+        }),
+        success: function (data, textStatus, xhr) {
+            successFunction();
+        },
+        error: function (xhr) {
+            errorFunction(xhr.status, xhr.responseText);
+        }
+    });
+}
+
+function deleteModelObjectByIdRequest(userType, userId, successFunction, errorFunction) {
+    $.ajax({
+        url: getApiRouteByUserType(userType) + userId,
+        method: "DELETE",
+        headers: {
+            'x-auth-token': localStorage.getItem('token')
+        },
+        success: function (data, textStatus, xhr) {
+            successFunction(data);
+        },
+        error: function (xhr) {
+            errorFunction(xhr.status, xhr.responseText);
+        }
+    });
+}
+//#endregion
+
+
+//#region Child Notes Requests
+
+export function getChildNotesRequest(childId, successFunction, errorFunction) {
+    $.ajax({
+        url: notesApi + childId,
+        method: "GET",
+        headers: {
+            'x-auth-token': localStorage.getItem('token')
+        },
+        success: function (data, textStatus, xhr) {
+            successFunction(data);
+        },
+        error: function (xhr) {
+            errorFunction(xhr.status, xhr.responseText);
+        }
+    });
+}
+
+export function postChildNotesRequest(childId, teacherId, noteContent, successFunction, errorFunction) {
+    $.ajax({
+        url: notesApi + childId,
+        method: "POST",
+        headers: {
+            "x-auth-token": localStorage.getItem("token")
+        },
+        data: JSON.stringify({
+            teacherId,
+            noteContent
+        }),
+        success: function (data, textStatus, xhr) {
+            successFunction(data);
+        },
+        error: function (xhr) {
+            errorFunction(xhr.status, xhr.responseText);
+        },
+    });
+}
+//#endregion
 
 
 export function getStatsRequest(childId, fieldId, successFunction, errorFunction) {
@@ -61,18 +316,37 @@ export function getStatsRequest(childId, fieldId, successFunction, errorFunction
     };
     $.ajax(request);
 }
-//@@authorization
-export function getChildsRequest(successFunction, errorFunction) {
+
+export function childRegisterRequest(newChild, successFunction, errorFunction) {
     let request = {
-        url: childsApi,
+      contentType: "application/json",
+      url: childsApi,
+      method: "POST",
+      headers: {
+        'x-auth-token': localStorage.getItem('token')
+      },
+      data: JSON.stringify(newChild),
+      success: function (data, textStatus, xhr) {
+        successFunction(data);
+      },
+      error: function (xhr) {
+        errorFunction(xhr.status, xhr.responseText);
+      },
+    };
+    $.ajax(request);
+  }
+
+  export function getGamesRequest(successFunction, errorFunction, completeFunction) {
+    let request = {
+        contentType: "application/json",
+        url: gamesApi,
         method: "GET",
         // headers: { 'x-auth-token' : localStorage.getItem('token') },
-        // data: JSON.stringify({}),
         success: function (data, textStatus, xhr) {
             successFunction(data);
         },
         error: function (xhr) {
-            errorFunction(xhr.status, xhr.responseText);
+            // errorFunction(xhr.status, xhr.responseText);
         }
     };
     $.ajax(request);
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,10 +4,14 @@ const _ = require('lodash'); // Pick/Select values from object
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-// const { User } = require('../models/user');
-const { Parent } = require('../models/parent');
-const { Teacher } = require('../models/teacher');
+const {
+    Parent
+} = require('../models/parent');
+const {
+    Teacher
+} = require('../models/teacher');
 
+const { customError } = require('../models/assets/customError');
 
 // Const Lengths [min_length, max_length]
 const ID_LEN = [2, 9];
@@ -16,20 +20,27 @@ const PASSWORD_LEN = [5, 1024];
 // HTTP Handling
 // POST ['api/auth/parent']       -   Login
 router.post('/parent', async (req, res) => {
+    console.log(req.body);
     // Validate client input
-    const { error } = validate(req.body);
+    const {
+        error
+    } = validateUserClaims(req.body);
     // Assert validation
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error)
+        return res.status(400).send(error.details[0].message);
     // Check if the user exist
-    let parent = await Parent.findOne({ id: req.body.id }).populate({
+    let parent = await Parent.findOne({
+        id: req.body.id
+    }).populate({
         path: 'children',
         select: 'id firstName lastName',
     });
-    // Response 400 Bad Request if the user not exist
-    if (!parent) return res.status(400).send(`לא קיים במערכת הורה בעל תעודת הזהות ${req.body.id}.`);
+    if (!parent) // Response 400 Bad Request if the user not exist
+        return res.status(400).send(`לא קיים במערכת הורה בעל תעודת הזהות ${req.body.id}.`);
     // Validate password
     const validPassword = await bcrypt.compare(req.body.password, parent.password);
-    if (!validPassword) return res.status(400).send("סיסמה שגויה.");
+    if (!validPassword)
+        return res.status(400).send("סיסמה שגויה.");
     // Create JWT
     const token = parent.generateAuthToken();
     // Send Response
@@ -41,20 +52,27 @@ router.post('/parent', async (req, res) => {
 // POST ['api/auth/teacher']       -   Login
 router.post('/teacher', async (req, res) => {
     // Validate client input
-    const { error } = validate(req.body);
+    const {
+        error
+    } = validateUserClaims(req.body);
     // Assert validation
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error)
+        return res.status(400).send(error.details[0].message);
     // Check if the user exist
-    let teacher = await Teacher.findOne({ id: req.body.id });
+    let teacher = await Teacher.findOne({
+        id: req.body.id
+    });
     // .populate({
     //     path: 'children',
     //     select: 'id firstName lastName',
     // });
-    // Response 400 Bad Request if the user not exist
-    if (!teacher) return res.status(400).send(`לא קיים במערכת מורה בעל תעודת הזהות ${req.body.id}.`);
+
+    if (!teacher) // Response 400 Bad Request if the user not exist
+        return res.status(400).send(`לא קיים במערכת מורה בעל תעודת הזהות ${req.body.id}.`);
     // Validate password
     const validPassword = await bcrypt.compare(req.body.password, teacher.password);
-    if (!validPassword) return res.status(400).send("סיסמה שגויה.");
+    if (!validPassword)
+        return res.status(400).send("סיסמה שגויה.");
     // Create JWT
     const token = teacher.generateAuthToken();
     // Send Response
@@ -64,65 +82,40 @@ router.post('/teacher', async (req, res) => {
 
 
 // Essential functions
-function validate(req) {
+function validateUserClaims(req) {
     const schema = {
-        id: Joi.string().min(ID_LEN[0]).max(ID_LEN[1]).required().error(errors => { return customError(errors, 'תעודת זהות') }),
-        password: Joi.string().min(PASSWORD_LEN[0]).max(PASSWORD_LEN[1]).required().error(errors => { return customError(errors, 'סיסמה') })
+        id: Joi.string().min(ID_LEN[0]).max(ID_LEN[1]).required().error(errors => {
+            return customError(errors, 'תעודת זהות')
+        }),
+        password: Joi.string().min(PASSWORD_LEN[0]).max(PASSWORD_LEN[1]).required().error(errors => {
+            return customError(errors, 'סיסמה')
+        })
     };
     return Joi.validate(req, schema);
 }
 
 
-function customError(errors, key) {
-    errors.forEach(err => {
-        switch (err.type) {
-            case 'any.empty':
-                err.message = `'${key}' לא יכול להיות ריק`;
-                break;
-            case 'any.required':
-                err.message = `'${key}' נדרש`;
-                break;
-            case 'string.min':
-                err.message = `'${key}' נדרש להכיל יותר מ-${err.context.limit} תוים`;
-                break;
-            case 'string.max':
-                err.message = `'${key}' נדרש להכיל פחות מ-${err.context.limit} תוים`;
-                break;
-            default:
-                break;
-        }
-    });
-    return errors;
-}
+// function customError(errors, key) {
+//     errors.forEach(err => {
+//         switch (err.type) {
+//             case 'any.empty':
+//                 err.message = `'${key}' לא יכול להיות ריק`;
+//                 break;
+//             case 'any.required':
+//                 err.message = `'${key}' נדרש`;
+//                 break;
+//             case 'string.min':
+//                 err.message = `'${key}' נדרש להכיל יותר מ-${err.context.limit} תוים`;
+//                 break;
+//             case 'string.max':
+//                 err.message = `'${key}' נדרש להכיל פחות מ-${err.context.limit} תוים`;
+//                 break;
+//             default:
+//                 break;
+//         }
+//     });
+//     return errors;
+// }
 
 // Module exports
 module.exports = router;
-
-
-
-// // POST ['api/auth']       -   Login
-// router.post('/', async (req, res) => {
-//     // Validate client input
-//     // const { error } = validate(req.body);
-//     // Assert validation
-//     // if(error)
-//     //     return res.status(400).send(error.details[0].message);
-//     // Check if the user exist
-//     let user = await User.findOne({ userId: req.body.userId }).populate({
-//         path: '_parent',
-//         populate: {
-//             path: 'children',
-//             select: '_id id firstName lastName'
-//         }
-//     });
-//     // Response 400 Bad Request if the user exist
-//     if (!user) return res.status(400).send("Invalid id or password.");
-//     // Validate password, bcypt.comare missing await
-//     const validPassword = await bcrypt.compare(req.body.password, user.password);
-//     if (!validPassword) return res.status(400).send("Invalid id or password.");
-//     // Create JWT
-//     const token = user.generateAuthToken();
-//     // Send Response
-//     res.header('x-auth-token', token);
-//     res.send(_.pick(user, ['_id', 'userId', '_parent']));
-// });
